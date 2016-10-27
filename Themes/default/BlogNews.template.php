1c5c49c551d8b2acc51017691a6acf270dc8d158
Jessica González <suki@missallsunday.com>
2016-10-26 20:10:19 -0500

    template and language files
    Signed-off-by: Jessica González <suki@missallsunday.com>

---
+++ Themes/default/BlogNews.template.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @package blogNews
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2015, Jessica González
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
---
+++ Themes/default/languages/BlogNews.english.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package blogNews
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2015, Jessica González
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+
+global $txt;
+
+$txt['BlogNews_'] = '';
